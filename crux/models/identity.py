a7c2c258937619bd154dd56a1fce1c46efa4252b
Anshul Patel <anshul.patel@cruxinformatics.com>
2019-09-03 20:11:46 +0530

    Adding changes

--- crux/models/identity.py
+++ crux/models/identity.py
@@ -39,6 +39,10 @@ class Identity(CruxModel):
         """str: Gets the Company name."""
         return self.raw_model["companyName"]
 
+    @company_name.setter
+    def company_name(self, company_name):
+        self.raw_model["companyName"] = company_name
+
     @property
     def description(self):
         """str: Gets the Description."""
@@ -49,30 +53,54 @@ class Identity(CruxModel):
         """str: Gets the First name."""
         return self.raw_model["firstName"]
 
+    @first_name.setter
+    def first_name(self, first_name):
+        self.raw_model["firstName"] = first_name
+
     @property
     def last_name(self):
         """str: Gets the Last name."""
         return self.raw_model["lastName"]
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self.raw_model["lastName"] = last_name
 
     @property
     def role(self):
         """str: Gets the Role."""
         return self.raw_model["role"]
 
+    @role.setter
+    def role(self, role):
+        self.raw_model["role"] = role
+
     @property
     def email(self):
         """str: Gets the Email."""
         return self.raw_model["email"]
+
+    @email.setter
+    def email(self, email):
+        self.raw_model["email"] = email
 
     @property
     def website(self):
         """str: Gets the Website."""
         return self.raw_model["website"]
 
+    @website.setter
+    def website(self, website):
+        self.raw_model["website"] = website
+
     @property
     def landing_page(self):
         """str: Gets the Landing Page."""
         return self.raw_model["landingPage"]
+
+    @landing_page.setter
+    def landing_page(self, landing_page):
+        self.raw_model["landingPage"] = landing_page
 
     @property
     def type(self):
@@ -83,6 +111,10 @@ class Identity(CruxModel):
     def phone(self):
         """str: Gets the phone."""
         return self.raw_model["phone"]
+
+    @phone.setter
+    def phone(self, phone):
+        self.raw_model["phone"] = phone
 
     def to_dict(self):
         # type: () -> Dict[str, Any]
